2e3cfb8c0b09cc6d31c2b68ec081fa17f55c420b
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-08 19:23:56 +0200

    passer des variables aux vues

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']);
+
+// Route::get('posts', function(){
+//     return response()->json([
+//         'title' => 'un titre',
+//         'description' => 'une description'
+//     ]);
+// });
+
+// Route::get('articles', function(){
+//     return view('articles');
+// });
