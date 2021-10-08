e6d190fa41d2803890709917d425d0c18044bf65
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-08 20:20:10 +0200

    utilisation de la function asset pour linker le fichier css et js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,11 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mon super site</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 
 <body>
     @include('partials.navbar')
     @yield('content')
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
