b5e8f5d3ca0fa12c48472f262b687d3a7ec1abf5
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-10 09:58:33 +0200

    displays post comments

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
     <h1>{{ $post->content }}</h1>
+
+    <hr>
+    @foreach ($post->comments as $comment)
+        <div>{{ $comment->content }}</div>
+    @endforeach
+
 @endsection
