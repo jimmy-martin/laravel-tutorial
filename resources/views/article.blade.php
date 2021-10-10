f71a07eff30c07de6349babc06543ba21564c697
jimmy-martin <jimmy.martin952@gmail.com>
2021-10-10 10:31:25 +0200

    show image path if exists

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -3,6 +3,10 @@
 @section('content')
     <h1>{{ $post->content }}</h1>
 
+    <span>
+        {{ $post->image ? $post->image->path : 'Pas d\'image !' }}
+    </span>
+
     <hr>
     @forelse ($post->comments as $comment)
         <div>{{ $comment->content }} | crée le {{ $comment->created_at->format('d/m/Y') }} </div>
